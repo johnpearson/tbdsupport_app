5f7a1655fd329876df848f77f7b61e424a105ecc
jpearson <jpearson556@gmail.com>
2011-06-30 08:24:53 -0600

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+Welcome to the Technology by Design Online Support Application!
+
+This application is designed to offer clients of Technology by Design a web application that handles online support requests while also providing a web interface for employees of the company to keep track of jobs.
+
+This application is under development.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TbdsupportApp::Application.config.secret_token = 'b34fe56e6a9dc15f5a325cf304e4739b904466f9d0086564c413b0e41f3182677352e715f877ca75bfbd0942df974c15f4412fca00cff06bfd85e5062d2f6e4d'
