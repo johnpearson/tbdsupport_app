2eb484a9dc6c4488ca82da13358bd0e97a0491f5
jpearson <jpearson556@gmail.com>
2011-07-14 16:03:12 -0600

    Minor changes to mail setup

--- config/initializers/setup_mail.rb
+++
@@ -1,7 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address          => "smtp.accountsupport.com",
-  :port             => "25",
-  :authentication   => :plain,
-  :user_name        => "support@techbydsn.com",
-  :password         => "techbydsn"
-}
